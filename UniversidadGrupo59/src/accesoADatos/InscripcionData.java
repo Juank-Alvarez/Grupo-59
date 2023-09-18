1ae2494cece4f9ffb5060e76b873cdb8a52d5905
FATY23 <fasegyra@gmail.com>
2023-09-18 17:30:44 -0300

    materiasNoCursadas

--- UniversidadGrupo59/src/accesoADatos/InscripcionData.java
+++ UniversidadGrupo59/src/accesoADatos/InscripcionData.java
@@ -188,5 +188,29 @@ public class InscripcionData {
 
         return materias;
     }
+      
+      public List<Materia> obtenerMateriasNoCursadas(int idAlumno){
+         ArrayList<Materia> materias=new ArrayList<>();
+         String sql="SELEC * FROM materia WHERE estado =1 AND idMateria not in"
+                 +"SELECT idMateria FROM inscripcion WHERE idAlumno =?";
+          
+         try{
+             PreparedStatement ps=con.prepareStatement (sql);
+             ps.setInt(1,idAlumno);
+             ResultSet rs=ps.executeQuery();
+             while(rs.next()){
+                 Materia materia=new Materia();
+                 materia.setIdMateria(rs.getInt("IdMateia"));
+                 materia.setNombre(rs.getString("nombre"));
+                 materia.setAnioMateria(rs.getInt("año"));
+                 materias.add(materia);
+             }
+             ps.close();
+         } catch (SQLException ex){
+             JOptionPane.showMessageDialog(null, "Error al accede a la tabla inscripcion");
+             
+         }     
+         return materias;
+     }
 
 }
